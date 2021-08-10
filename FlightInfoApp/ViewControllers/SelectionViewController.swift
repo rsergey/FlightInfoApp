ac83fce67ba619d18307290dc278d395366c90be
Sergey <rumyantsev.serge@yandex.ru>
2021-08-10 21:56:02 +0300

    new way to introduce iata code

--- FlightInfoApp/ViewControllers/SelectionViewController.swift
+++ FlightInfoApp/ViewControllers/SelectionViewController.swift
@@ -7,16 +7,23 @@
 
 import UIKit
 
-class SelectionViewController: UIViewController {
+class SelectionViewController: UIViewController, UITextFieldDelegate {
+    // MARK: - IBOutlets
+    @IBOutlet weak var iataTextField: UITextField!
     
     // MARK: - Private Properties
-    let accessKey = "bf9f1644f60ea683c4a27c95035f65ab"
-    var airportIata = ""
+    private let accessKey = "bf9f1644f60ea683c4a27c95035f65ab"
+    private var airportIata = ""
 
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        iataTextField.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -34,20 +41,35 @@ class SelectionViewController: UIViewController {
         }
     }
     
-    // MARK: - IBActions
-    @IBAction func airportSelectionButtonTapped(_ sender: UIButton) {
-        switch sender.tag {
-        case 0:
-            airportIata = "LED"
-        case 1:
-            airportIata = "SVO"
-        case 2:
-            airportIata = "DME"
-        default:
-            airportIata = "VKO"
+    // MARK: - Pablic Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (iataTextField.text?.count ?? 0) > 2 {
+            airportIata = iataTextField.text ?? ""
+            performSegue(withIdentifier: "showFlightsSegue", sender: nil)
+        } else {
+            showAlert()
         }
-        performSegue(withIdentifier: "showFlightsSegue", sender: nil)
+        return true
     }
     
-}
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let set = NSCharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzyz").inverted
+        let maxLength = 3
+        let currentString = (textField.text ?? "") as NSString
+        let newString : NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        return (string.rangeOfCharacter(from: set) == nil) && (newString.length <= maxLength)
+    }
+    
+    // MARK: - Private Methods
+    private func showAlert() {
+        let alert = UIAlertController(title: "Wrong IATA code!",
+                                      message: "IATA aiport code contains three letters.",
+                                      preferredStyle: .alert)
+        let okButtonAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.iataTextField.becomeFirstResponder()
+        }
+        alert.addAction(okButtonAction)
+        present(alert, animated: true)
+    }
 
+}
