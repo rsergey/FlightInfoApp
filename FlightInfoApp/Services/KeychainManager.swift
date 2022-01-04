8febb893a890845500a9bc2a6d50092f1926f128
Sergey <rumyantsev.serge@yandex.ru>
2022-01-04 21:04:33 +0300

    Implementation of Keychain class methods

--- FlightInfoApp/Services/KeychainManager.swift
+++ FlightInfoApp/Services/KeychainManager.swift
@@ -5,6 +5,7 @@
 //  Created by Sergey on 18.12.2021.
 //
 
+import Foundation
 import Security
 
 class KeychainManager {
@@ -15,20 +16,88 @@ class KeychainManager {
     private init() {}
     
     // MARK: - Public Methods
-    func saveItem(apiKey: String) {
+    func save(data: String, service: String, account: String) {
+        let value: Data = data.data(using: .utf8) ?? Data()
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecClass as String: kSecClassGenericPassword,
+            kSecValueData as String: value as AnyObject
+        ]
+        let status = SecItemAdd(query as CFDictionary, nil)
         
+        guard status == errSecSuccess else {
+            print("Save to keychain failed")
+            return
+        }
+        print("Save to keychain success")
     }
     
-    func getItem() {
+    func update(data: String, service: String, account: String) {
+        let value: Data = data.data(using: .utf8) ?? Data()
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecClass as String: kSecClassGenericPassword]
+        let attributes: [String: AnyObject] = [
+            kSecValueData as String: value as AnyObject
+        ]
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
         
+        guard status != errSecItemNotFound else {
+            print("Item not found")
+            return
+        }
+        
+        guard status == errSecSuccess else {
+            print("Keychain item update failed")
+            return
+        }
+        print("Keychain item update success")
     }
     
-    func updateItem(apiKey: String) {
+    func read(service: String, account: String) -> String? {
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecClass as String: kSecClassGenericPassword,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnData as String: kCFBooleanTrue
+        ]
         
+        var itemCopy: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &itemCopy)
+        
+        guard status != errSecItemNotFound else {
+            print("Item not found")
+            return nil
+        }
+        
+        guard status == errSecSuccess else {
+            print("Keychain item read failed")
+            return nil
+        }
+        
+        guard let item = itemCopy as? String else {
+            return nil
+        }
+        
+        return item
     }
     
-    func deliteItem() {
+    func delete(service: String, account: String) {
+        let query: [String: AnyObject] = [
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecClass as String: kSecClassGenericPassword,
+        ]
+        let status = SecItemDelete(query as CFDictionary)
         
+        guard status == errSecSuccess else {
+            print("Delete from keychain failed")
+            return
+        }
+        print("Delete from keychain success")
     }
     
 }
