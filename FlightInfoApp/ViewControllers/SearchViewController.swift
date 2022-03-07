e06c4fd3775cb4d317e4bfeb0d2bd8488badf308
Sergey <rumyantsev.serge@gmail.com>
2022-03-07 20:59:21 +0300

    SearchViewController init

---
+++ FlightInfoApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  FlightInfoApp
+//
+//  Created by Sergey Rumyantsev on 07.03.2022.
+//
+
+import UIKit
+
+class SearchViewController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
